7c08250cf58b56127cb3e20e799fb40b7d28386d
eyzee <azizihamza1910@gmail.com>
2023-10-30 22:30:55 +0100

    Added a function that checks if there is a cycle in a linked list using Army and scouts analogy

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the given linked list has a cycle in it
+ * Using "army" and "scouts" metaphor(the army and scouts start from
+ * the same position. The scouts move twice faster than
+ * the army, if there is a cycle in the list, the scout will
+ * eventually end up meeting with the army again which is only
+ * possible if there is a cycle in the list)
+ * @list: a pointer to the head of the list
+ * Return: (1) if there is a cycle, (2) if there is no cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *army, *scouts;
+
+	army = list;
+	scouts = list;
+	printf("Army and scouts are now separated\n");
+	while (scouts != 0x0 && scouts->next != 0x0)
+	{
+		army = army->next;
+		scouts = scouts->next->next;
+		if (army == scouts)
+		{
+			printf("Army and scouts have met again\n");
+			printf("How are we supposed to win if this keeps happening!\n");
+			return (1);
+		}
+	}
+	return (0);
+}
