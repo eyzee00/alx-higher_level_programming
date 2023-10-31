fe875bf50f3893ec09c37ba3ccc08872517e876f
eyzee <azizihamza1910@gmail.com>
2023-10-31 22:06:07 +0100

    Added missing return statement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number *= -1
+        number = number * (-1)
     print("{:d}".format(number % 10), end="")
+    return number % 10
+print_last_digit(98)
+print_last_digit(0)
+print_last_digit(1444)
