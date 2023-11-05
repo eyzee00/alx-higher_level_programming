655b0f67dd0b25d85c136b38081b7db979381134
eyzee <azizihamza1910@gmail.com>
2023-11-05 17:43:50 +0100

    Added a function that replaces an element in a list by anew value

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def replace_in_list(mylist, idx, new_value):
+    if idx < 0:
+        return mylist
+    elif idx >= len(mylist):
+        return mylist
+    else:
+        mylist[idx] = new_value
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4]
+    replace_in_list(my_list, 0, -54)
+    replace_in_list(my_list, 2, -54)
+    print(my_list)
