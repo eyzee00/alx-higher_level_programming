f4ea650785707fc94f5a41bae40767a0a48b80a0
eyzee <azizihamza1910@gmail.com>
2023-11-07 00:59:45 +0100

    Addeda function in C that prints info about python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,18 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
-
+/**
+ * print_python_list_info - prints infor about python lists
+ * @p: the python list we want info about
+ * Return; (void)
+ */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size_p, allocated, idx = 0;
-	PyObject *element;
+	Py_ssize_t size = 0, allocated_space = 0, counter = 0;
+	PyObject *item = NULL;
 
-	size_p = PyList_Size(p);
-	allocated = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", size_p);
-	printf("[*] Allocated = %ld\n", allocated);
-	while (idx < size_p)
+	size = PyList_Size(p);
+	allocated_space = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated_space);
+	while (counter < size)
 	{
-		element = PyList_GET_ITEM(p, idx);
-		printf("Element %ld: %s\n", idx, element->ob_type->tp_name);
-		idx++;
+		item = PyList_GET_ITEM(p, counter);
+		printf("Element %ld: %s\n", counter, item->ob_type->tp_name);
+		counter = counter + 1;
 	}
 }
