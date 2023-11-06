2983e535daca127053003cfa17658f95cab7af2c
eyzee <azizihamza1910@gmail.com>
2023-11-07 00:54:16 +0100

    !

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,27 +1,18 @@
-#include <stdio.h>
-#include "Python.h"
-/**
- * print_python_list_info - prints info about a python list
- * @p: list object
- */
+#include <Python.h>
+
 void print_python_list_info(PyObject *p)
 {
-    int i;
-    PyListObject *pp;
-    /*
-     * lists in python are of the type PyListObject. PyObject is just a type
-     * used to pass other types around. So you will need to cast your object back
-     * to its oringinal type
-     */
-    pp = (PyListObject *)p;
+	Py_ssize_t size_p, allocated, idx = 0;
+	PyObject *element;
 
-    printf("[*] Size of the Python List = %ld\n", pp->ob_base.ob_size);
-    printf("[*] Allocated = %ld\n", pp->allocated);
-
-    for (i = 0; i < pp->ob_base.ob_size; i++)
-    {
-        printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
-
-    }
+	size_p = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size_p);
+	printf("[*] Allocated = %ld\n", allocated);
+	while (idx < size_p)
+	{
+		element = PyList_GET_ITEM(p, idx);
+		printf("Element %ld: %s\n", idx, element->ob_type->tp_name);
+		idx++;
+	}
 }
-
