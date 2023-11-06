97119513a94dffe8a492f65255b7acba88805ff2
eyzee <azizihamza1910@gmail.com>
2023-11-06 21:22:18 +0100

    !

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,10 @@
 def divisible_by_2(mylist=[]):
     boolist = mylist[:]
     i = 0
-    index = 0
-    while index < len(mylist):
+    while i < len(mylist):
         if not mylist[i] % 2:
             boolist[i] = True
         else:
             boolist[i] = False
         i += 1
-        index += 1
     return boolist
