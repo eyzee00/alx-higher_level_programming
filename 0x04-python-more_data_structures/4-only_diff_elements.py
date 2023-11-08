cd987655d4b554bba8d7cd74366b83f2c280a847
eyzee <azizihamza1910@gmail.com>
2023-11-08 20:27:35 +0100

    !

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    unique_list = []
-    [unique_list.append(i) for i in set_1 if elem not in set_2]
-    [unique_list.append(i) for i in set_2 if elem not in set_1]
-    return unique_list
+    set_3 = set(set_1.symmetric_difference(set_2))
+    return set_3
