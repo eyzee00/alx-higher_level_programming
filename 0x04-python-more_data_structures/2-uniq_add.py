27eb55e4b45fb26d1cf2e74dbf7b9288b21be6a2
eyzee <azizihamza1910@gmail.com>
2023-11-08 19:58:49 +0100

    Added a function that adds all unique integers in a list and returns it

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_list = []
+    for element in my_list:
+        if unique_list == []:
+            unique_sum = element
+            unique_list.append(element)
+        else:
+            unique = True
+            for i in unique_list:
+                if i == element:
+                    unique = False
+                    break
+            if unique:
+                unique_sum += element
+                unique_list.append(element)
+    return unique_sum
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print("Result: {:d}".format(result))
