d11909a8a23d20786a80f263a6d9c2e9610461ee
eyzee <azizihamza1910@gmail.com>
2023-11-08 20:43:35 +0100

    !

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary={}, key, value):
-    return a_dictionary.update(key, value)
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        del a_dictionary[key]
+        a_dictionary.update(key, value)
+    else:
+        a_dictionary.update(key, value)
+    return a_dictionary
