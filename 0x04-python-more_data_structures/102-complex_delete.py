6ab763d358a5bcfb2196c8cf55bd7ead2b00fad6
eyzee <azizihamza1910@gmail.com>
2023-11-09 00:16:07 +0100

    !

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, target):
-    while value in a_dictionary.values():
+    while target in a_dictionary.values():
         for key, value in a_dictionary.items():
             if value is target:
                 del a_dictionary[key]
