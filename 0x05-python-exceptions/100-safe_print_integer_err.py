0c3d590380e1d3165833b2b6f08c629b2e79c4a7
eyzee <azizihamza1910@gmail.com>
2023-11-20 22:03:24 +0100

    !

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    value = 89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
