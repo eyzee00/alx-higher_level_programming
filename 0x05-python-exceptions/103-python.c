3e333962b82f8e522e1711adca27ef42e4ee8b31
eyzee <azizihamza1910@gmail.com>
2023-11-20 22:51:00 +0100

    !

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -10,8 +10,8 @@ void print_python_float(PyObject *p)
 	double value = 0;
 	char *string = NULL;
 
+	fflush(stdout);
 	printf("[.] float object info\n");
-
 	if (PyFloat_CheckExact(p) == NULL)
 	{
 		printf("  [ERROR] Invalid Float Object\n");
@@ -33,14 +33,13 @@ void print_python_bytes(PyObject *p)
 	Py_ssize_t size = 0, i = 0;
 	char *string = NULL;
 
+	fflush(stdout);
 	printf("[.] bytes object info\n");
-
 	if (PyBytes_CheckExact(p) == NULL)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
 	size = PyBytes_Size(p);
 	printf("  size: %zd\n", size);
 	string = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
@@ -64,6 +63,7 @@ void print_python_list(PyObject *p)
 	PyObject *item;
 	int i = 0;
 
+	fflush(stdout);
 	printf("[*] Python list info\n");
 	if (PyList_CheckExact(p))
 	{
