3379c961602630cd22e99c731105978699f5556d
eyzee <azizihamza1910@gmail.com>
2023-11-21 20:05:48 +0100

    Added appropriate Exceotion messages

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size=0):
         """__init__: constructs a Square object"""
         if isinstance(size, int) is False:
-            raise TypeError
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
