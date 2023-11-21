1dc55a00b600a5be7d82fcb7b4a6e1dd60597a3b
eyzee <azizihamza1910@gmail.com>
2023-11-21 19:24:56 +0100

    Fixed errors

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,21 @@ class Square:
     """Square: defines a square object"""
     def __init__(self, size):
         """__init__: constructs a square object"""
-        self.size = size
-        """size: private attribute"""
+        self.__size = size
+        """__size: private attribute"""
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
