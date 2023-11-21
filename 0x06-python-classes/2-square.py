ace4c67a6a0c64e137451ee3bfa0d16cda7d2c7e
eyzee <azizihamza1910@gmail.com>
2023-11-21 20:04:14 +0100

    !

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,26 +10,5 @@ class Square:
             raise TypeError
         elif size < 0:
             raise ValueError
-        self.__size = size
-
-    def area(self):
-        """area: returns the area of a Square object"""
-        return self.__size ** 2
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
+        else:
+            self.__size = size
