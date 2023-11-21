02ee30a014732b6d90f15573507a1f99618fd858
eyzee <azizihamza1910@gmail.com>
2023-11-21 21:21:23 +0100

    !

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,10 @@ class Square:
         else:
             self.__size = size
 
-    @size.getter
+    def area(self):
+        return self.__size ** 2
+
+    @property
     def size(self):
         return self.__size
 
@@ -25,3 +28,17 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+
+if __name__ == "__main__":
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)
