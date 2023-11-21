fc915d0674413a8ce3238d81205ac24f75eaf722
eyzee <azizihamza1910@gmail.com>
2023-11-21 20:52:30 +0100

    !

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module: defines a Square class"""
+
+
+class Square:
+    """Square: defines a square object"""
+    def __init__(self, size=0):
+        """__init__: constructs a square object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
