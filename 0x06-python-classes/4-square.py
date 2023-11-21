4f1db7b69359bf6a79da548b1e8f1932c2e1e3f7
eyzee <azizihamza1910@gmail.com>
2023-11-21 21:16:16 +0100

    !

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module: defines a class Square"""
+
+
+class Square:
+    """Square: defines a Square object"""
+    def __init__(self, size=0):
+        """__init__: constructs a square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @size.getter
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
