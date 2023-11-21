3e4c47944a9e8fbd9329ca2b7ca400ae5c1a1601
eyzee <azizihamza1910@gmail.com>
2023-11-21 19:11:32 +0100

    !

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Module: defines a Square object"""
 class Square:
+    """Square: defines a square object"""
     pass
+
+
+if __name__ == "__main__":
+    square = Square()
+    print(square)
