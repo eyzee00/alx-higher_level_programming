d373f76659fcec958a0fd4141674a4568ef46ac7
eyzee <azizihamza1910@gmail.com>
2023-11-27 22:43:11 +0100

    !

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    print(Rectangle.print_symbol, end="")
+                    print(self.print_symbol, end="")
                 print("")
 
     def __str__(self):
@@ -52,7 +52,7 @@ class Rectangle:
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    print(Rectangle.print_symbol, end="")
+                    print(self.print_symbol, end="")
                 if i < self.__height - 1:
                     print("")
             return ""
@@ -95,3 +95,29 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+
+if __name__ == "__main__":
+    my_rectangle_1 = Rectangle(8, 4)
+    print(my_rectangle_1)
+    print("--")
+    my_rectangle_1.print_symbol = "&"
+    print(my_rectangle_1)
+    print("--")
+
+    my_rectangle_2 = Rectangle(2, 1)
+    print(my_rectangle_2)
+    print("--")
+    Rectangle.print_symbol = "C"
+    print(my_rectangle_2)
+    print("--")
+
+    my_rectangle_3 = Rectangle(7, 3)
+    print(my_rectangle_3)
+
+    print("--")
+
+    my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+    print(my_rectangle_3)
+
+    print("--")
