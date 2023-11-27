5c8fc44e671c109170c382e99013f9f08f46d445
eyzee <azizihamza1910@gmail.com>
2023-11-27 20:36:13 +0100

    !

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module: rectangle class"""
+
+class Rectangle:
+    """Rectangle: defines a rectangle object"""
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
