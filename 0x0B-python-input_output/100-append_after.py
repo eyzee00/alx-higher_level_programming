2be748de22eb65da0dee5324b82f25299d32835f
eyzee00 <azizihamza1910@gmail.com>
2023-12-06 00:05:02 +0100

    !

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module: defines a function that inserts a line of 
+"""Module: defines a function that inserts a line of
 text to a file, after each line containing a specific string"""
 
 
