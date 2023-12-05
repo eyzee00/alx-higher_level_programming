c011f43318d46ec425b1468a32fcb5a4fd88e314
eyzee00 <azizihamza1910@gmail.com>
2023-12-05 22:37:27 +0100

    !

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Module: defines a function that adds all arguments
+"""Module: defines a script that adds all arguments
 to a Python list, and then save them to a file"""
 
+from sys import argv
+import os
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
 
 
-def add_item(filename, my_list=[]):
-    """Adds all arguments to a Python list, and
-    then save them to a text file"""
-    new_list = load(filename)
-    new_list.extend(my_list)
-    save(new_list, filename)
-    return new_list
+if __name__ == "__main__":
+    if os.path.exists("add_item.json"):
+        my_list = load("add_item.json")
+    else:
+        my_list = []
+    for i in range(1, len(argv)):
+        my_list.append(argv[i])
+    save(my_list, "add_item.json")
