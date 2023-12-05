6b035e743ed93f0fb010f812b40eb9d3af9e82ea
eyzee00 <azizihamza1910@gmail.com>
2023-12-05 22:43:14 +0100

    !

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ to a Python list, and then save them to a file"""
 import sys
 
 if __name__ == "__main__":
-    save = __import__('5-save_to_json_file').save
-    load = __import__('6-load_from_json_file').load
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load("add_item.json")
