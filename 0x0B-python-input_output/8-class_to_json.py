ae5d34ba9f128cfe645fafb6b10a59b26605eab5
eyzee00 <azizihamza1910@gmail.com>
2023-12-05 22:46:53 +0100

    !

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,7 @@
 """Module: defines a function that returns the JSON
 representation of the given class"""
 
-import json
-
 
 def class_to_json(obj):
-    """Returns the JSON representation of a class"""
-    return json.dumps(obj.__dict__)
+    """Returns the JSON representation of an object (string)"""
+    return obj.__dict__
