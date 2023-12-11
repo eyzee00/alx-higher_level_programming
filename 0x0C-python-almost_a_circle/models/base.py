04a71d3de6aca33349ebca90de0b8b12345238c9
eyzee00 <azizihamza1910@gmail.com>
2023-12-11 21:17:29 +0100

    !

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module: defines a base class for all models"""
+
+
+class Base:
+    """Base: mother class for all models"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
