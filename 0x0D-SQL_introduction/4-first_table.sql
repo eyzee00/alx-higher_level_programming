7344c628c485c0cfe2aa578e0d8fd72fdea904aa
eyzee00 <azizihamza1910@gmail.com>
2024-01-16 23:47:44 +0100

    !

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
  -- this script creates a table in the current database
- CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+ CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
