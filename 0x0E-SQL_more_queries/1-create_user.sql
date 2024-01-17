51f05d094bf32d3c9119249521690a082e9f912d
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 00:24:11 +0100

    !

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- this script creates a user and grants him all privileges
 CREATE USER
-	IF NOT EXISTS `user_0d_1`@`localhost`
-	IDENTIFIED BY `user_0d_1_pwd`;
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.* TO `user_0d_1`@`localhost`
-	IDENTIFIED BY `user_0d_1_pwd`;
+	ON *.* TO 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
