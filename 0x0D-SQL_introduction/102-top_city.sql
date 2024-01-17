9b5e62e0209498c819c13f73223a7967756fa286
eyzee00 <azizihamza1910@gmail.com>
2024-01-17 01:20:13 +0100

    !

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- lists the top three cities with the highest average temperature
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
