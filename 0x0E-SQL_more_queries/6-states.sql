55d2e0ee4e7b3cb64a97f87d1b6f773b3392ff45
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 00:53:25 +0100

    !

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- this script the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states 
 	(PRIMARY KEY(id),
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256));
