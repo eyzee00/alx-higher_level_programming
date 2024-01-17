d45c467cbe789ab84ae5eddd68f993028ec591be
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 00:58:42 +0100

    !

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 	state_id INTEGER NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY(state_id) REFERENCES
-	hbtn_0d_usa.state(id));
+	hbtn_0d_usa.states(id));
