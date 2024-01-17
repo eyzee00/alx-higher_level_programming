7e1d137fc724090be737538aaf22ddb4761a620b
eyzee00 <azizihamza1910@gmail.com>
2024-01-17 01:07:26 +0100

    !

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
 -- this scripts converts first_table to utf8
-USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
