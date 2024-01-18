819af20680b7c0b4b7a99f8775f4b9281a7636db
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 01:24:27 +0100

    !

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- this script lists all shows 
+-- contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+SELECT sh.title, ge.genre_id FROM tv_shows AS sh
+	INNER JOIN tv_shows_genres AS ge
+	ON sh.id = ge.show_id
+	ORDER BY sh.title, ge.genre_id;
