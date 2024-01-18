c8b7758bd448c355beca7bbda80943b01deb1dea
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 01:26:07 +0100

    !

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- contained in hbtn_0d_tvshows
 -- that have at least one genre linked
 SELECT sh.title, ge.genre_id FROM tv_shows AS sh
-	INNER JOIN tv_shows_genres AS ge
+	INNER JOIN tv_show_genres AS ge
 	ON sh.id = ge.show_id
 	ORDER BY sh.title, ge.genre_id;
