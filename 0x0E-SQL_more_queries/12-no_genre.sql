3e396a55adf4e56df2f75c77b18b1119fcdbf1c8
eyzee00 <azizihamza1910@gmail.com>
2024-01-18 01:34:50 +0100

    !

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- this script lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT sh.title, ge.genre_id FROM tv_shows AS sh
 	LEFT JOIN tv_show_genres AS ge
-	ON sh.id = ge.genre_id
+	ON sh.id = ge.show_id
 	WHERE ge.genre_id IS NULL
 	ORDER BY sh.title, ge.genre_id;
