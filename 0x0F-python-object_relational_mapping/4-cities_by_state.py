98190bca0506961f5fc34234ea794245290f1f17
eyzee00 <azizihamza1910@gmail.com>
2024-02-19 00:48:00 +0100

    !

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: defines a script that lists all states with names
+like the given argument safely
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = database.cursor()
+    sql = "SELECT * FROM `cities` ORDER BY id"
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+            print(row)
