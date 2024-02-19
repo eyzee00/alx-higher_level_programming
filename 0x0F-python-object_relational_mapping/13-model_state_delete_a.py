a65308588d0a97f5f5cfd4e973637194248489ed
eyzee00 <azizihamza1910@gmail.com>
2024-02-19 02:23:09 +0100

    !

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,7 +7,7 @@ containing a in their name
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sys import argv 
+from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
