dd95a7fbd8a6316b0b2a8b34e9881baa81fb1166
eyzee00 <azizihamza1910@gmail.com>
2024-02-19 03:58:35 +0100

    !

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -4,9 +4,9 @@ Module: deletes all the state rows (objects)
 containing a in their name
 """
 
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for row in session.query(State):
-        if "a" in row.name:
-            session.delete(row)
-    session.commit()
+    if session.query(State) is not None:
+        for row in session.query(State):
+            if "a" in row.name:
+                session.delete(row)
+                session.commit()
