cfb5808fc003fec9964da408bbe258889c000b03
eyzee00 <azizihamza1910@gmail.com>
2024-03-02 01:53:01 +0100

    f

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         argv[2], argv[1])
 
-    request= get(url)
+    request = get(url)
     commits = request.json()
     try:
         for i in range(10):
